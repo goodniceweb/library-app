b2c9b80529e2a5d10a6d274454ed8f6f253eb8ea
Alexey Cherkashin <goodniceweb@gmail.com>
2016-05-30 21:09:13 +0300

    Finalize http://yoember.com/ tutorial

---
+++ app/models/book.js
@@ -0,0 +1,35 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+import Faker from 'faker';
+
+export default Model.extend({
+  title: attr('string'),
+  releaseYear: attr('date'),
+
+  library: belongsTo('library'),
+  author: belongsTo('author'),
+
+  isNotValid: Ember.computed.empty('title'),
+
+  randomize(author, library) {
+    this.set('title', this._bookTitle());
+    this.set('author', author);
+    this.set('releaseYear', this._randomYear());
+    this.set('library', library);
+
+    return this;
+  },
+
+  _bookTitle() {
+    return `${Faker.commerce.productName()} Cookbook`;
+  },
+
+  _randomYear() {
+    return new Date(this._getRandomArbitrary(1900, 2015));
+  },
+
+  _getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+});
